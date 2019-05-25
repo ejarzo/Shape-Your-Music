3c507cab90e7f9e8fa91ee240b49971e54298e7e
ejarzo <ejarz25@gmail.com>
2019-05-25 15:46:33 -0400

    add check to prevent users from updating projects they dont own

--- functions/project-update.js
+++ functions/project-update.js
@@ -4,11 +4,29 @@ const { q, client } = getFauna();
 exports.handler = async (event, context) => {
   const data = JSON.parse(event.body);
   const id = getId(event.path);
+  const { user } = context.clientContext;
   console.log(`Function 'project-update' invoked. update id: ${id}`);
+
+  if (!user) {
+    return {
+      statusCode: 403,
+    };
+  }
+
+  const project = await client.query(q.Get(q.Ref(`classes/projects/${id}`)));
+  const { userId } = project.data;
+
+  if (userId !== user.id) {
+    return {
+      statusCode: 403,
+    };
+  }
+
   try {
     const response = await client.query(
       q.Update(q.Ref(`classes/projects/${id}`), { data })
     );
+
     console.log('success', response);
     return {
       statusCode: 200,

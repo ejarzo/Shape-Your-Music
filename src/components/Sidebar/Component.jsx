6da5585eee54a400f31388186fec42ea67aaca3f
Marouane Rassili <mrassili@aol.com>
2019-10-03 15:56:09 +0100

    Refactor Sidebar to use hooks

--- src/components/Sidebar/Component.jsx
+++ src/components/Sidebar/Component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles.module.css';
@@ -9,33 +9,19 @@ const propTypes = {
   downloadUrls: PropTypes.array.isRequired,
 };
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      iconScale: 1,
-    };
-  }
+function Sidebar({ activePage, downloadUrls, handleTabClick }) {
+  const [iconScale, setIconScale] = useState(1);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.downloadUrls.length !== this.props.downloadUrls.length) {
-      this.setState({
-        iconScale: 1.8,
-      });
-      setTimeout(() => {
-        this.setState({
-          iconScale: 1,
-        });
-      }, 200);
-    }
-  }
+  useEffect(() => {
+    setIconScale(1.8);
+    const timer = setTimeout(() => setIconScale(1), 200);
+    return () => clearTimeout(timer);
+  }, [downloadUrls]);
 
-  render() {
-    const { activePage, downloadUrls, handleTabClick } = this.props;
-    return (
-      <div className={styles.sidebar}>
-        <div className={styles.tabs}>
-          {/*<div
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.tabs}>
+        {/*<div
             className={styles.tabButton}
             title="Settings"
             style={{
@@ -48,45 +34,44 @@ class Sidebar extends React.Component {
             </div>
           </div>*/}
 
+        <div
+          className={styles.tabButton}
+          title="Downloads"
+          style={{
+            background: activePage === 1 && 'white',
+          }}
+          onClick={() => handleTabClick(1)}
+        >
           <div
-            className={styles.tabButton}
-            title="Downloads"
             style={{
-              background: activePage === 1 && 'white',
+              transition: 'all 0.2s',
+              transform: `scale(${iconScale})`,
             }}
-            onClick={() => handleTabClick(1)}
           >
-            <div
-              style={{
-                transition: 'all 0.2s',
-                transform: `scale(${this.state.iconScale})`,
-              }}
-            >
-              <i className="ion-android-download" />
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div className={styles.content}>
-            {activePage === 0 && (
-              <div className={styles.contentInner}>
-                <h2>Settings</h2>
-                <label htmlFor="projectName">Project Name</label>
-                <br />
-                <input name="projectName" />
-              </div>
-            )}
-            {activePage === 1 && (
-              <div className={styles.contentInner}>
-                <Downloads downloadUrls={downloadUrls} />
-              </div>
-            )}
+            <i className="ion-android-download" />
           </div>
         </div>
       </div>
-    );
-  }
+
+      <div>
+        <div className={styles.content}>
+          {activePage === 0 && (
+            <div className={styles.contentInner}>
+              <h2>Settings</h2>
+              <label htmlFor="projectName">Project Name</label>
+              <br />
+              <input name="projectName" />
+            </div>
+          )}
+          {activePage === 1 && (
+            <div className={styles.contentInner}>
+              <Downloads downloadUrls={downloadUrls} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 Sidebar.propTypes = propTypes;

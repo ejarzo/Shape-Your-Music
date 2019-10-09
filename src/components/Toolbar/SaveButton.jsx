60f50d838a5595afd74fc5b80de84f013ac82a24
ejarzo <ejarz25@gmail.com>
2019-10-09 13:41:57 -0400

    use hooks on SaveButton

--- src/components/Toolbar/SaveButton.jsx
+++ src/components/Toolbar/SaveButton.jsx
@@ -1,54 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Popconfirm, Input, Icon } from 'antd';
 import Button from 'components/Button';
 import { appColors } from 'utils/color';
 
 const MAX_NAME_LENGTH = 30;
 
-class SaveButton extends React.Component {
-  constructor(props) {
-    super(props);
-    const { projectName } = props;
-    this.state = { projectName };
-  }
+function SaveButton(props) {
+  const { projectName, onConfirm } = props;
+  const [newProjectName, setProjectName] = useState(
+    projectName || 'My Project'
+  );
 
-  render() {
-    const { onConfirm } = this.props;
-    const { projectName } = this.state;
-    console.log(projectName);
+  const handleSubmit = e => {
+    newProjectName ? onConfirm(newProjectName) : alert('Please enter a name');
+  };
 
-    return (
-      <div>
-        <Popconfirm
-          title={
-            <div style={{ maxWidth: 200 }}>
-              <div style={{ marginBottom: 15 }}>
-                What is your project named?
-              </div>
+  return (
+    <div>
+      <Popconfirm
+        title={
+          <div style={{ maxWidth: 200 }}>
+            <div style={{ marginBottom: 15 }}>
+              {projectName
+                ? `Overwrite "${projectName}"?`
+                : 'What is your project named?'}
+            </div>
+            {!projectName && (
               <Input
                 autoFocus
-                value={projectName}
-                defaultValue={projectName}
+                value={newProjectName}
+                onPressEnter={handleSubmit}
                 onChange={({ target: { value } }) => {
-                  if (value.length < MAX_NAME_LENGTH)
-                    this.setState({ projectName: value });
+                  if (value.length < MAX_NAME_LENGTH) setProjectName(value);
                 }}
               />
-            </div>
-          }
-          placement="bottomRight"
-          okText="Save"
-          cancelText="Cancel"
-          onConfirm={() => onConfirm(projectName)}
-          icon={<Icon type="question-circle-o" />}
-        >
-          <Button hasBorder color={appColors.grayLightest} title="Save project">
-            Save
-          </Button>
-        </Popconfirm>
-      </div>
-    );
-  }
+            )}
+          </div>
+        }
+        placement="bottomRight"
+        okText="Save"
+        cancelText="Cancel"
+        onConfirm={handleSubmit}
+        icon={<Icon type="question-circle-o" />}
+      >
+        <Button hasBorder color={appColors.grayLightest} title="Save project">
+          Save
+        </Button>
+      </Popconfirm>
+    </div>
+  );
 }
 
 export default SaveButton;

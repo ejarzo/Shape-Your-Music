1db9f7340d184f75c9361cdef45340f4eda5af89
ejarzo <ejarz25@gmail.com>
2019-10-22 18:56:40 -0400

    update react-select to latest version, update styling logic

--- src/components/CustomSelect/index.jsx
+++ src/components/CustomSelect/index.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Select from 'react-select';
 import PropTypes from 'prop-types';
 
-import { appColors, getDarker } from 'utils/color';
-import styles from './styles.module.css';
+import { appColors, getDarker, getLighter } from 'utils/color';
 
 const propTypes = {
   menuTop: PropTypes.bool,
@@ -16,76 +15,106 @@ const propTypes = {
 
 function CustomSelect(props) {
   const { white, black, grayLightest } = appColors;
-  const backgroundColor = props.color || grayLightest;
+  const { color } = props;
+  const backgroundColor = color || grayLightest;
 
-  const borderColor = props.color ? getDarker(props.color) : white;
-  const foreground = props.color ? white : black;
-  const fontWeight = props.color ? 'bold' : 'normal';
-  const arrowRenderer = ({ isOpen }) => (
-    <div
-      style={{
-        color: foreground,
-        fontSize: 15,
-      }}
-    >
-      {isOpen ? (
-        <i className="ion-chevron-up" />
-      ) : (
-        <i className="ion-chevron-down" />
-      )}
-    </div>
-  );
-  arrowRenderer.propTypes = { isOpen: PropTypes.bool };
-
-  const position = props.menuTop
-    ? {
-        top: 'auto',
-        bottom: 'calc(100% + 2px)',
-      }
-    : {
-        bottom: 'auto',
-        top: 'calc(100% + 3px)',
-      };
+  const borderColor = color ? getDarker(color) : white;
+  const foreground = color ? white : black;
+  const fontWeight = color ? 'bold' : 'normal';
 
   return (
     <Select
-      {...props}
-      className={styles.customSelect}
-      wrapperStyle={{ height: '100%', fontWeight }}
-      menuContainerStyle={{
-        marginBottom: 'none',
-        ...position,
-      }}
-      optionClassName={props.color ? styles.lightText : styles.darkText}
-      valueRenderer={value => (
-        <div
-          title={props.title}
-          style={{
-            backgroundColor: backgroundColor,
+      menuPlacement={props.menuTop ? 'top' : 'bottom'}
+      styles={{
+        menuPortal: base => ({ ...base, zIndex: 9999 }),
+        menu: base => ({
+          ...base,
+          border: `2px solid ${borderColor}`,
+          background: backgroundColor,
+          // margin: '1px 0',
+          borderRadius: 1,
+          padding: 0,
+          boxShadow: '-5px 0 12px 0 rgba(0,0,0,0.11)',
+        }),
+        menuList: base => ({
+          ...base,
+          padding: 0,
+        }),
+        container: base => ({
+          ...base,
+          minHeight: 28,
+          padding: 0,
+          height: '100%',
+          width: '100%',
+          border: 'none',
+          outline: 'none',
+        }),
+        control: (base, { isFocused }) => ({
+          ...base,
+          boxShadow: 'none',
+          height: '100%',
+          width: '100%',
+          minHeight: 0,
+          padding: '0 8px',
+          backgroundColor: isFocused
+            ? getLighter(backgroundColor)
+            : backgroundColor,
+          border: color ? 'none' : `1px solid ${getDarker(backgroundColor)}`,
+          borderRadius: !color && 1,
+          '&:hover': {
+            backgroundColor: getLighter(backgroundColor),
+          },
+        }),
+        valueContainer: base => ({
+          ...base,
+          padding: 0,
+        }),
+        singleValue: base => ({
+          ...base,
+          fontWeight,
+          color: foreground,
+        }),
+        dropdownIndicator: base => ({
+          ...base,
+          color: foreground,
+          '&:hover': {
             color: foreground,
-            height: '100%',
-            width: '100%',
-            padding: 3,
-            paddingTop: 4,
-            paddingLeft: 6,
-            display: 'grid',
-            alignItems: 'center',
-            border: !props.color && `1px solid ${getDarker(backgroundColor)}`,
-            borderRadius: !props.color && 1,
-          }}
-        >
-          {value.label}
-        </div>
-      )}
-      arrowRenderer={arrowRenderer}
-      menuStyle={{
-        border: `2px solid ${borderColor}`,
-        background: backgroundColor,
-        margin: -3,
-        boxShadow: '-5px 0 12px 0 rgba(0,0,0,0.11)',
+          },
+        }),
+        indicatorsContainer: base => ({
+          ...base,
+          padding: 0,
+          height: '100%',
+        }),
+        indicatorContainer: base => ({
+          ...base,
+          padding: 0,
+        }),
+        option: (base, { isFocused, isSelected }) => ({
+          ...base,
+          padding: '5px 8px',
+          fontWeight,
+          color: foreground,
+          background: isSelected
+            ? color
+              ? getLighter(color, 0.3)
+              : 'rgba(0, 0, 0, 0.1)'
+            : isFocused && (color ? getLighter(color) : 'rgba(0, 0, 0, 0.05)'),
+        }),
       }}
-      searchable={false}
-      clearable={false}
+      menuPortalTarget={document.body}
+      isSearchable={false}
+      closeMenuOnScroll={true}
+      // menuShouldBlockScroll={true}
+      components={{
+        DropdownIndicator: () => (
+          <div style={{ fontSize: 15 }}>
+            <i className="ion-chevron-down" />
+          </div>
+        ),
+        IndicatorSeparator: null,
+      }}
+      isClearable={false}
       value={props.options.find(({ value }) => value === props.value)}
       options={props.options}
       onChange={props.onChange}

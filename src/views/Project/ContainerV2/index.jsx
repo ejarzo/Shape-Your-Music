a57594e877fa837eeab8c12109fffcc1284d3d98
Elias Jarzombek <ejarz25@gmail.com>
2020-06-26 16:28:51 -0400

    fix warning

--- src/views/Project/ContainerV2/index.jsx
+++ src/views/Project/ContainerV2/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import Teoria from 'teoria';
 import { HotKeys } from 'react-hotkeys';
 

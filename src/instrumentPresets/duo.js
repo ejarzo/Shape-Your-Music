0ea7d6e7a4464ded12fbfd30e1050ab3bf3a03af
Elias Jarzombek <ejarz25@gmail.com>
2023-03-08 13:20:56 -0500

    fix build

--- src/instrumentPresets/duo.js
+++ src/instrumentPresets/duo.js
@@ -1,13 +1,6 @@
 import * as Tone from 'tone';
 import { setSynthParam, setEffectWet } from './utils';
 
-const duoSyn = new Tone.DuoSynth({
-  voice0: {
-    oscillator: {
-      type: 'amsine',
-    },
-  },
-});
 export default {
   name: 'Duo',
   baseSynth: Tone.DuoSynth,
@@ -57,42 +50,42 @@ export default {
     },
   },
   effects: [
-    // {
-    //   type: Tone.Chorus,
-    //   params: {
-    //     frequency: 1.5,
-    //     delayTime: 3.5,
-    //     depth: 0.9,
-    //     feedback: 0.1,
-    //     type: 'sine',
-    //     spread: 180,
-    //   },
-    // },
+    {
+      type: Tone.Chorus,
+      params: {
+        frequency: 1.5,
+        delayTime: 3.5,
+        depth: 0.9,
+        feedback: 0.1,
+        type: 'sine',
+        spread: 180,
+      },
+    },
   ],
   dynamicParams: [
-    // {
-    //   name: 'glide',
-    //   default: 0,
-    //   target: 'instrument',
-    //   func: setSynthParam('portamento', 0, 0.5),
-    // },
-    // {
-    //   name: 'chorus',
-    //   default: 20,
-    //   target: 'effect',
-    //   func: setEffectWet(0),
-    // },
-    // {
-    //   name: 'vibrato',
-    //   default: 10,
-    //   target: 'instrument',
-    //   func: setSynthParam('vibratoAmount', 0, 1),
-    // },
-    // {
-    //   name: 'harmonicity',
-    //   default: 0,
-    //   target: 'instrument',
-    //   func: setSynthParam('harmonicity', 0, 3),
-    // },
+    {
+      name: 'glide',
+      default: 0,
+      target: 'instrument',
+      func: setSynthParam('portamento', 0, 0.5),
+    },
+    {
+      name: 'chorus',
+      default: 20,
+      target: 'effect',
+      func: setEffectWet(0),
+    },
+    {
+      name: 'vibrato',
+      default: 10,
+      target: 'instrument',
+      func: setSynthParam('vibratoAmount', 0, 1),
+    },
+    {
+      name: 'harmonicity',
+      default: 0,
+      target: 'instrument',
+      func: setSynthParam('harmonicity', 0, 3),
+    },
   ],
 };

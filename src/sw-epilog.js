9a2c74171ba06ddf0c2d6544b1ca7956d835c4eb
Elias Jarzombek <ejarz25@gmail.com>
2023-03-08 13:46:46 -0500

    test sw

--- src/sw-epilog.js
+++ src/sw-epilog.js
@@ -1,8 +1,12 @@
-addEventListener('message', event => {
+/* Custom service worker epilog */
+self.addEventListener('message', event => {
   console.log(`Message received: ${event.data}`);
   if (event.data === 'NEW_CONTENT_AVAILABLE') {
     if (window.confirm('New Version Available. Please refresh now.')) {
       window.location.reload();
     }
+  }
+  if (event.data === 'CONTENT_CACHED') {
+    console.log('Content is cached');
   }
 });
